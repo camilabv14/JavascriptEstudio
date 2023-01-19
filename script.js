893140232a194d0992fe60602a2edbe8d05e15dd
camilabv14 <camilabv14@hotmail.com>
2023-01-18 22:49:49 -0500

    result submit button added

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const h1 = document.querySelector("h1");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
 const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
 
 function btnOnClick() {
-  console.log(parseInt(input1.value) + parseInt(input2.value)) 
+  const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+  pResult.innerText = 'Resultado: ' + sumaInputs;
 }
